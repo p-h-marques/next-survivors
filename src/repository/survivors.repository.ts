7f4aaf8acc6bb5c402237d96fba4d97c531ee6a6
Pedro Henrique <dg.pedromarques@gmail.com>
2022-05-02 09:44:04 -0300

    feat: 🎸 handling search with filters

--- src/repository/survivors.repository.ts
+++ src/repository/survivors.repository.ts
@@ -1,5 +1,26 @@
-import { IFilters } from '../models';
+import { EnumFilters, IFilters } from '../models';
+
+const url = 'http://localhost:3000/api/';
+
+function getInfectedStatus(select: EnumFilters): boolean | undefined {
+  const status = {
+    [EnumFilters.ALL]: undefined,
+    [EnumFilters.INFECTED]: 'true',
+    [EnumFilters.CURED]: 'false',
+  };
+
+  return status[select] || undefined;
+}
 
 export async function getSurvivors(filters: IFilters) {
-  console.log('get survivors', filters);
+  const name = filters.input;
+  const infected = getInfectedStatus(filters.select);
+  const query = `?name=${name}&${
+    infected !== undefined ? 'infected=' + infected : ''
+  }`;
+
+  const request = await fetch(url + 'survivors' + query);
+  const response = await request.json();
+
+  console.log(response);
 }
