7dba2595490ad2e8616672492f2180ebf13dfc3f
Pedro Henrique <dg.pedromarques@gmail.com>
2022-05-02 01:11:50 -0300

    style: 💄 filter styles

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -1,15 +1,73 @@
+import { useState } from 'react';
 import Image from 'next/image';
 import ImgLogo from '../../../assets/logo.svg';
-import { NavStyles } from './styles';
+import ImgDropdown from '../../../assets/dropdown.svg';
+import { FilterStyles, MainStyles, NavStyles } from './styles';
+
+enum EnumFilters {
+  ALL = 'all survivors',
+  INFECTED = 'infected survivors',
+  CURED = 'cured survivors',
+}
 
 export default function HomePage() {
+  const [filter, setFilter] = useState(EnumFilters.ALL);
+  const [isDropdownVisible, setDropdownVisibility] = useState(false);
+
   return (
     <>
       <NavStyles>
         <Image src={ImgLogo} />
         <strong>Survivors: Zombie Apocalypse</strong>
       </NavStyles>
-      <main>teste2</main>
+      <MainStyles>
+        <FilterStyles>
+          <input type="text" placeholder="filter survivor by name" />
+          <div
+            className="selector"
+            onClick={() => setDropdownVisibility(!isDropdownVisible)}
+          >
+            <span>{filter}</span>
+            <Image
+              src={ImgDropdown}
+              style={{
+                transform: isDropdownVisible ? 'rotate(180deg)' : 'unset',
+              }}
+            />
+            <div
+              className="dropdown"
+              style={{
+                display: isDropdownVisible ? 'block' : 'none',
+              }}
+            >
+              <div
+                className="dropdown__option"
+                onClick={() => {
+                  setFilter(EnumFilters.ALL);
+                }}
+              >
+                {EnumFilters.ALL}
+              </div>
+              <div
+                className="dropdown__option"
+                onClick={() => {
+                  setFilter(EnumFilters.INFECTED);
+                }}
+              >
+                {EnumFilters.INFECTED}
+              </div>
+              <div
+                className="dropdown__option"
+                onClick={() => {
+                  setFilter(EnumFilters.CURED);
+                }}
+              >
+                {EnumFilters.CURED}
+              </div>
+            </div>
+          </div>
+        </FilterStyles>
+      </MainStyles>
     </>
   );
 }
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,13 @@
 export const theme = {
   colors: {
     background: '#202020',
+    background_hover: '#181818',
     light: '#FFFFFF',
+    medium: '#4B4B4B',
+    danger: '#AE3333',
+    danger_hover: '#7D2828',
+    background_danger: '#372323',
+    cured: '#265B1D',
+    cured_hover: '#193A13',
   },
 };
